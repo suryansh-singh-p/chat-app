2ab2ac65d859936d5212c85d695f5b08bdf174ef
Suryansh <singhsuryansh28parihar@gmail.com>
2025-06-03 11:05:01 +0530

    minor updates

--- public/src/pages/Login.jsx
+++ public/src/pages/Login.jsx
@@ -101,7 +101,7 @@ const FormContainer = styled.div`
   justify-content: center;
   gap: 1rem;
   align-items: center;
-  background-color: #131324;
+  background-color: #131323;
   .brand {
     display: flex;
     align-items: center;
